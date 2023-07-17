1f16ebe0060d12953cb66fa43ce844a394b17c2b
ArkadiNazarian <nazarianarkadi@gmail.com>
2023-07-17 16:16:28 +0330

    full calendar tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interaction from '@fullcalendar/interaction';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <FullCalendar
+        plugins={[ dayGridPlugin,interaction ]}
+        initialView="dayGridMonth"
+        events={[
+          { title: 'event 1',description:"1", date: '2023-03-28' },
+          // { title: 'event 2', date: '2023-03-28',backgroundColor:"#ffffff" }
+        ]}
+        eventClick={()=>console.log("g")}
+        editable={true}
+        droppable={true}
+        eventDrop={(e)=>console.log(e.event.start)}
+        dateClick={(e)=>console.log(e)}
+      />
     </div>
   );
 }
